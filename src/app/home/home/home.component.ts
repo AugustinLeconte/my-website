f3b7d745fa3f90d76ed7753fb57b676a6bdcf712
IT-Misericordia-Chile <tichile@misericordia.fr>
2025-07-10 21:27:02 -0400

    adding new component and list of ideas for the future

--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { ProjectListComponent } from '../project-list/project-list.component';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ProjectListComponent],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
-
-}
+export class HomeComponent {}
